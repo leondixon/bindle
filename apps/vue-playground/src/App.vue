fbd48527c187d26910bd580c9470e1e36b1079c4
Leon Dixon <me@leondixon.com>
2025-03-01 16:03:11 +0000

    fixing repo structure

--- apps/vue-playground/src/App.vue
+++ apps/vue-playground/src/App.vue
@@ -1,7 +1,51 @@
 <script setup lang="ts">
+import type { BindleSchema } from '@bindle/schema'
+import { SchemaRender } from '@bindle/vue'
 
+const schema: BindleSchema = {
+  $element: 'input',
+  id: 'input',
+  name: 'input',
+}
+
+const schemaArray: BindleSchema = [
+  {
+    $element: 'input',
+    id: 'input',
+    name: 'input',
+  },
+  {
+    $element: 'textarea',
+    id: 'textarea',
+    name: 'textarea',
+  },
+]
+
+const schemaArray2: BindleSchema = [
+  {
+    $element: 'div',
+    children: [
+      {
+        $element: 'p',
+        children: 'Test text',
+      },
+    ],
+  },
+  {
+    $element: 'input',
+    id: 'input',
+    name: 'input',
+  },
+  {
+    $element: 'textarea',
+    id: 'textarea',
+    name: 'textarea',
+  },
+]
 </script>
 
 <template>
-  app
+  <SchemaRender :schema="schema" />
+  <SchemaRender :schema="schemaArray" />
+  <SchemaRender :schema="schemaArray2" />
 </template>
