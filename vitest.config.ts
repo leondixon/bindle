b42176c088305a425156e34074f37d9e3e4e7969
Leon Dixon <me@leondixon.com>
2025-09-19 17:45:56 +0100

    added docs

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,12 +1,12 @@
-import { defineConfig } from "vitest/config";
-import vue from '@vitejs/plugin-vue'
-export default defineConfig({
-  plugins: [vue()],
-  test: {
-    environment: 'jsdom',
-    coverage: {
-      provider: 'v8',
-    },
-    globals: true
-  }
-})
+// import { defineConfig } from "vitest/config";
+// import vue from '@vitejs/plugin-vue'
+// export default defineConfig({
+//   plugins: [vue()],
+//   test: {
+//     environment: 'jsdom',
+//     coverage: {
+//       provider: 'v8',
+//     },
+//     globals: true
+//   }
+// })
--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -1,11 +1,11 @@
-import { defineWorkspace } from 'vitest/config'
 
-export default defineWorkspace([
-  "packages/*",
-  "apps/*",
-  {
-    test: {
-      exclude: ['**/eslint.config.ts/**','**/eslint.config.js/**', '**/node_modules/**']
-    }
-  }
-])
+// export default defineWorkspace([
+//   "packages/*",
+//   "apps/*",
+//   "docs",
+//   {
+//     test: {
+//       exclude: ['**/eslint.config.ts/**','**/eslint.config.js/**', '**/node_modules/**']
+//     }
+//   }
+// ])
